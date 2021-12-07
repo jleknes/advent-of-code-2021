476c616371c2f01c52c3d37a2c4d2175f886d768
Jørund Leknes <jorund.leknes@gmail.com>
2021-12-07 12:24:20 +0100

    day7: improved using binary search

--- day7/threacheryofwhales.py
+++ day7/threacheryofwhales.py
@@ -19,14 +19,15 @@ step_costs = {}
 
 def generate_step_costs(max_val):
     for i in range(max_val):
-        step_costs[i]=step_cost(i)
+        step_costs[i]=int((i*(i+1))/2)
     return
 
-def step_cost(x):
-    sum = 0
-    for i in range(1,x+1):
-        sum+=i
-    return sum
+def total_cost_position(positions, i):
+    cost = 0
+    for j in range (len(positions)):
+        distance = abs(positions[j]-i)
+        cost+=step_costs[distance]
+    return cost
 
 def part_two():
     start_time = time.time()
@@ -38,15 +39,46 @@ def part_two():
     generate_step_costs(positions[len(positions)-1]+1)
 
     lowest_cost = 9223372036854775807
+
     for i in range(positions[len(positions)-1]):
-        cost = 0
-        for j in range (len(positions)):
-            distance = abs(positions[j]-i)
-            cost+=step_costs[distance]
-        if (cost<lowest_cost):
+        cost = total_cost_position(positions, i)
+        if (cost<lowest_cost):            
             lowest_cost=cost
+        print(i, cost)
     print(lowest_cost)
     print("--- %s seconds ---" % (time.time() - start_time))
 
+# binary search for lowest value
+def iter_search(positions):
+    l = 0
+    r = len(positions)
+    while (l <= r) :
+        m = int(l + (r - l) / 2) # m = (l + r) / 2
+        if ((r == l + 1) and total_cost_position(positions, l) <= total_cost_position(positions, r)):
+            return total_cost_position(positions, l)
+        
+        if ((r == l + 1) and total_cost_position(positions, l) > total_cost_position(positions, r)):
+            return total_cost_position(positions, r)
 
-part_one()
+        if (total_cost_position(positions, m) < total_cost_position(positions, m + 1) and total_cost_position(positions, m) < total_cost_position(positions, m - 1)):
+            return total_cost_position(positions, m)
+
+        if (total_cost_position(positions, m) < total_cost_position(positions, m + 1) and total_cost_position(positions, m) > total_cost_position(positions, m - 1)):
+            r = m - 1
+        else :
+            l = m + 1
+    return -1
+
+def part_two_binary_search():
+    start_time = time.time()
+    for line in fileinput.input():
+        positions = list(map(int, line.split(',')))
+        pass
+    positions.sort()
+    generate_step_costs(positions[len(positions)-1]+1)
+
+    lowest_cost = iter_search(positions)
+    print(lowest_cost)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+part_two_binary_search()
